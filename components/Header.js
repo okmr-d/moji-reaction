ade2fbf6addb8b9e6cb924ea8da40e6b45399fdd
Daiki Okumura <daiki.okumura@gmail.com>
2020-12-25 18:58:09 +0900

    react-hooks-form 追加

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="border-b">
+      <div className="max-w-2xl mx-auto">
+        <div className="flex justify-between">
+          <div className="flex px-4">
+            <a className="h-12 flex items-center">サイト名</a>
+          </div>
+          <div className="flex-grow flex justify-end px-4">
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,62 @@
 import Head from 'next/head'
+import { useForm } from "react-hook-form";
+
+import Header from '../components/Header'
+
 
 export default function Home() {
+
+  const { register, handleSubmit, errors } = useForm()
+
+  const onSubmit = ({ text, font }) => {
+    console.log({ text, font })
+  }
+
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>サイト名</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
+
+      <div className="max-w-2xl mx-auto">
+        <div className="p-4">
+          <form onSubmit={handleSubmit(onSubmit)}>
+
+            <div>
+              <div><label htmlFor="text">テキスト</label></div>
+              <div>
+                <textarea name="text" placeholder="" ref={register({ required: true })} />
+              </div>
+            </div>
+
+            <div>
+              <div><label>フォント</label></div>
+              <div>
+                <div>
+                  <label>
+                    <input name="font" type="radio" value="font1" ref={register({ required: true })} />
+                    font1
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    <input name="font" type="radio" value="font2" ref={register({ required: true })} />
+                    font2
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <input type="submit" />
+            </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
