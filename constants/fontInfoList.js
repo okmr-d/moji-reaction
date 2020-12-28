cb52191f849cd1dfc1eac9d8633ee8c4746166e6
Daiki Okumura <daiki.okumura@gmail.com>
2020-12-29 08:34:01 +0900

    フォームのデザイン修正

--- constants/fontInfoList.js
+++ constants/fontInfoList.js
@@ -1,33 +1,43 @@
 export const fontInfoList = {
   'noto-sans-jp-bold': {
     name: 'noto-sans-jp-bold',
-    nameJa: '太めゴシック',
+    nameJa: '太ゴシック',
     ext: 'otf',
-    adjustedValue: 80
+    adjustedValue: 80,
+    fontFamily: "'Noto Sans JP', sans-serif",
+    fontWeight: 700,
   },
   'mplus-rounded-1c-bold': {
     name: 'mplus-rounded-1c-bold',
-    nameJa: '太め丸ゴシック',
+    nameJa: '太丸ゴシック',
     ext: 'ttf',
-    adjustedValue: 60
+    adjustedValue: 60,
+    fontFamily: "'M PLUS Rounded 1c', sans-serif",
+    fontWeight: 700,
   },
   'noto-sans-jp-black': {
     name: 'noto-sans-jp-black',
     nameJa: '極太ゴシック',
     ext: 'otf',
-    adjustedValue: 80
+    adjustedValue: 80,
+    fontFamily: "'Noto Sans JP', sans-serif",
+    fontWeight: 900,
   },
   'mplus-rounded-1c-black': {
     name: 'mplus-rounded-1c-black',
     nameJa: '極太丸ゴシック',
     ext: 'ttf',
-    adjustedValue: 60
+    adjustedValue: 60,
+    fontFamily: "'M PLUS Rounded 1c', sans-serif",
+    fontWeight: 900,
   },
   'noto-serif-jp-black': {
     name: 'noto-serif-jp-black',
     nameJa: '極太明朝',
     ext: 'otf',
-    adjustedValue: 80
+    adjustedValue: 80,
+    fontFamily: "'Noto Serif JP', serif",
+    fontWeight: 900,
   },
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useForm } from "react-hook-form"
+import { useForm, useWatch } from "react-hook-form"
 
 import Header from '../components/Header'
 import ColorPicker from '../components/ColorPicker'
-import { fontInfoList } from '../constants/fontInfoList'
+import { fontInfoList, defaultFontInfo } from '../constants/fontInfoList'
 
 export default function Home() {
-  const [color, setColor] = useState("#EC71A1")
   const [colorModalOpen, setColorModalOpen] = useState(false)
 
   const [shareInfo, setShareInfo] = useState({
@@ -22,9 +21,19 @@ export default function Home() {
 
   const router = useRouter()
 
-  const { register, handleSubmit, errors } = useForm()
+  // Form
+  const { register, handleSubmit, errors, control, setValue } = useForm({
+    defaultValues: {
+      text: "",
+      font: defaultFontInfo.name,
+      color: "#EC71A1",
+    },
+  })
+  register('color', { required: true })
+  const currentFont = useWatch({ control, name: 'font' })
+  const currentColor = useWatch({ control, name: 'color' })
 
-  const onSubmit = ({ text, font }) => {
+  const onSubmit = ({ text, font, color }) => {
     const queryString = `text=${encodeURIComponent(text)}&font=${font}&color=${color.replace('#', '')}`
     const pagePath = `/?${queryString}`
     const imagePath = `/api/image?${queryString}`
@@ -49,31 +58,57 @@ export default function Home() {
         <meta name="twitter:card" content="summary" />
         <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_ROOT_URL + shareInfo.pagePath} />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700;900&family=M+PLUS+Rounded+1c:wght@700;900&family=Noto+Serif+JP:wght@900&display=swap" rel="stylesheet" />
+        </Head>
 
       <Header />
 
       <div className="max-w-2xl mx-auto">
         <div className="p-4">
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="sm:flex sm:-mx-2">
-              <div className="sm:w-2/5 sm:px-2">
+            <div className="sm:flex sm:-mx-4">
+              <div className="sm:w-5/12 sm:px-4">
                 <div className="mb-6">
-                  <div className="font-bold mb-2">1. テキストを入力</div>
+                  <div className="font-bold mb-2">1. テキストを入力 (改行可)</div>
                   <div>
-                    <textarea name="text" placeholder="" ref={register({ required: true })} />
+                    <textarea
+                      name="text"
+                      placeholder="いい&#13;ね！"
+                      ref={register({ required: true })}
+                      className={`block appearance-none text-base bg-white border-2 border-gray-200 rounded w-full py-1.5 px-2.5 focus:outline-none focus:border-gray-500 ${errors.text ? 'border-red-500 bg-red-100' : ''}`}
+                    />
                   </div>
                   {errors.text?.type === "required" && <div className="mt-1 text-xs text-red-500">入力してください</div>}
                 </div>
 
                 <div className="mb-6">
                   <div className="font-bold mb-2">2. フォントを選択</div>
-                  <div>
+                  <div className="">
                     {Object.keys(fontInfoList).map(key => (
                       <div key={key}>
-                        <label>
-                          <input name="font" type="radio" value={fontInfoList[key].name} ref={register({ required: true })} />
-                          {fontInfoList[key].nameJa}
+                        <label
+                          className={`
+                            block mb-1.5 px-4 py-1 border-2 rounded-full cursor-pointer
+                            ${fontInfoList[key].name === currentFont ? 'border-blue-500 bg-blue-100' : ''}
+                          `}
+                        >
+                          <input
+                            name="font"
+                            type="radio"
+                            value={fontInfoList[key].name}
+                            ref={register({ required: true })}
+                            className="hidden"
+                          />
+                          <span
+                            style={{
+                              fontFamily: fontInfoList[key].fontFamily,
+                              fontWeight: fontInfoList[key].fontWeight
+                            }}
+                          >
+                            {fontInfoList[key].nameJa}
+                          </span>
                         </label>
                       </div>
                     ))}
@@ -84,15 +119,15 @@ export default function Home() {
                 <div className="mb-6">
                   <div className="font-bold mb-2">3. 文字色を選択</div>
                   <a
-                    className="inline-flex items-center px-3 py-1.5 border rounded cursor-pointer hover:bg-gray-100"
-                    style={{ color: color }}
+                    className="inline-flex items-center px-3 py-1.5 border-2 rounded cursor-pointer hover:bg-gray-100"
+                    style={{ color: currentColor }}
                     onClick={() => setColorModalOpen(true)}
                   >
                     <div
                       className="h-6 w-6 rounded border border-white"
-                      style={{ backgroundColor: color }}
+                      style={{ backgroundColor: currentColor }}
                     />
-                    <div className="ml-2">文字色</div>
+                    <div className="ml-2">{currentColor}</div>
                   </a>
 
                 </div>
@@ -107,8 +142,10 @@ export default function Home() {
                       style={{ width: '248px' }}
                     >
                       <ColorPicker
-                        color={color}
-                        onChange={setColor}
+                        color={currentColor}
+                        onChange={(color) => {
+                          setValue('color', color)
+                        }}
                       />
                       <div className="mt-6">
                         <a
@@ -122,7 +159,7 @@ export default function Home() {
                   </div>
                 )}
               </div>
-              <div className="sm:w-3/5 sm:px-2">
+              <div className="sm:w-7/12 sm:px-4">
 
                 <div className="mb-6">
                   <div className="font-bold mb-2">4. プレビュー</div>
@@ -161,7 +198,7 @@ export default function Home() {
                         <span className="mr-0.5">
                           <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24"><g><path d="M11.96 14.945c-.067 0-.136-.01-.203-.027-1.13-.318-2.097-.986-2.795-1.932-.832-1.125-1.176-2.508-.968-3.893s.942-2.605 2.068-3.438l3.53-2.608c2.322-1.716 5.61-1.224 7.33 1.1.83 1.127 1.175 2.51.967 3.895s-.943 2.605-2.07 3.438l-1.48 1.094c-.333.246-.804.175-1.05-.158-.246-.334-.176-.804.158-1.05l1.48-1.095c.803-.592 1.327-1.463 1.476-2.45.148-.988-.098-1.975-.69-2.778-1.225-1.656-3.572-2.01-5.23-.784l-3.53 2.608c-.802.593-1.326 1.464-1.475 2.45-.15.99.097 1.975.69 2.778.498.675 1.187 1.15 1.992 1.377.4.114.633.528.52.928-.092.33-.394.547-.722.547z"></path><path d="M7.27 22.054c-1.61 0-3.197-.735-4.225-2.125-.832-1.127-1.176-2.51-.968-3.894s.943-2.605 2.07-3.438l1.478-1.094c.334-.245.805-.175 1.05.158s.177.804-.157 1.05l-1.48 1.095c-.803.593-1.326 1.464-1.475 2.45-.148.99.097 1.975.69 2.778 1.225 1.657 3.57 2.01 5.23.785l3.528-2.608c1.658-1.225 2.01-3.57.785-5.23-.498-.674-1.187-1.15-1.992-1.376-.4-.113-.633-.527-.52-.927.112-.4.528-.63.926-.522 1.13.318 2.096.986 2.794 1.932 1.717 2.324 1.224 5.612-1.1 7.33l-3.53 2.608c-.933.693-2.023 1.026-3.105 1.026z"></path></g></svg>
                         </span>
-                        <span>{process.env.NEXT_PUBLIC_SITE_ROOT_URL}</span>
+                        <span>{process.env.NEXT_PUBLIC_SITE_ROOT_URL.replace(/https?:\/\//, '')}</span>
                       </div>
                     </div>
                   </div>
@@ -177,7 +214,7 @@ export default function Home() {
                   <div>
                     <button
                       onClick={() => {
-                        window.open(`https://twitter.com/share?url=${process.env.NEXT_PUBLIC_SITE_ROOT_URL + shareInfo.pagePath}`)
+                        window.open(`https://twitter.com/share?url=${encodeURIComponent(process.env.NEXT_PUBLIC_SITE_ROOT_URL + shareInfo.pagePath)}`)
                       }}
                       disabled={!shareInfo.canShare}
                       className="bg-blue-500"
